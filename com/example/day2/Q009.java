d1fa0b79d84c16c4264a50d1320b95f23d0ee133
ssoGroup <sojeong5167@gmail.com>
2025-04-28 17:04:21 +0900

    day2  test

---
+++ com/example/day2/Q009.java
@@ -0,0 +1,19 @@
+package com.example.day2;
+
+import java.util.Scanner;
+
+/*
+* 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.*/
+public class Q009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if( n%2 ==0 ){
+            System.out.println(n + "is even");
+        } else {
+            System.out.println(n + "is odd");
+        }
+
+    }
+}
